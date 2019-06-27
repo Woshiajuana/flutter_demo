c0f217f43b1a15320524aa49cf61850e584e6c95
Woshiajuana <zhigang.chen@owulia.com>
2019-06-27 19:51:35 +0800

    sub code secodepahe.dart

--- lib/views/secondPage.dart
+++ lib/views/secondPage.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import '../components/input.dart';
 
 class SecondPage extends StatefulWidget {
   @override
@@ -7,15 +8,32 @@ class SecondPage extends StatefulWidget {
 }
 
 class SecondPageState extends State<SecondPage> {
+
+  TextEditingController controller;
+  String active = 'test';
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
           title: new Text('界面2')
       ),
-      body: new Center(
-        child: new Text('这是第二个界面'),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new TextField(
+              controller: controller,
+              onChanged: _onChanged,
+            ),
+            new Input(active: active),
+          ],
+        ),
       ),
     );
+  }
+  void _onChanged(String value) {
+    setState(() {
+      active = value;
+    });
   }
 }
