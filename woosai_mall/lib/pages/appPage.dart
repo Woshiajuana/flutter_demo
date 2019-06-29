4fcb17b9e82c568c2db40b9ebbde900a3c029bc0
Woshiajuana <zhigang.chen@owulia.com>
2019-06-29 17:00:40 +0800

    sub code

--- woosai_mall/lib/pages/appPage.dart
+++ woosai_mall/lib/pages/appPage.dart
@@ -11,18 +11,15 @@ class _AppPageState extends State<AppPage> with SingleTickerProviderStateMixin {
 
   // Tab页的控制器，可以用来定义Tab标签和内容页的坐标
   TabController tabController;
-  var tabArr = [
-    {
-      title: '首页',
-    }
-  ];
+  var tabTitles = [ '首页', '发现', '购物车', '我的' ];
+  var tabImages;
   int tabIndex = 0;
 
   @override
   void initState() {
     super.initState();
     tabController = new TabController(
-        length: tabArr.length, // Tab页的个数
+        length: tabTitles.length, // Tab页的个数
         vsync: this, // 动画效果的异步处理，默认格式
     );
   }
@@ -30,6 +27,7 @@ class _AppPageState extends State<AppPage> with SingleTickerProviderStateMixin {
   // 生命周期方法构建Widget时调用
   @override
   Widget build(BuildContext context) {
+    initData();
     return new Scaffold(
       body: new TabBarView(
         controller: tabController,
@@ -48,44 +46,16 @@ class _AppPageState extends State<AppPage> with SingleTickerProviderStateMixin {
           indicatorColor: Colors.white,
           controller: tabController,
           tabs: <Tab>[
-            new Tab(
-              child: Column(
-                children: <Widget>[
-                  new Image.asset('assets/images/app-home-icon.png'),
-                  new Text('首页')
-                ],
-              ),
-            ),
-            new Tab(
-              child: Column(
-                children: <Widget>[
-                  new Image.asset('assets/images/app-home-icon.png'),
-                  new Text('首页')
-                ],
-              ),
-            ),
-            new Tab(
-              child: Column(
-                children: <Widget>[
-                  new Image.asset('assets/images/app-home-icon.png'),
-                  new Text('首页')
-                ],
-              ),
-            ),
-            new Tab(
-              child: Column(
-                children: <Widget>[
-                  new Image.asset('assets/images/app-home-icon.png'),
-                  new Text('首页')
-                ],
-              ),
-            ),
+            new Tab(text: tabTitles[0], icon: getTabIcon(0)),
+            new Tab(text: tabTitles[1], icon: getTabIcon(1)),
+            new Tab(text: tabTitles[2], icon: getTabIcon(2)),
+            new Tab(text: tabTitles[3], icon: getTabIcon(3)),
           ],
-          labelColor: Color(0xffAAA5B2),
-          unselectedLabelColor: Color(0xffAAA5B2),
+          labelColor: Color(0xff1296db),
+          unselectedLabelColor: Color(0xff999999),
           onTap: (index) {
             setState(() {
-              _tabIndex = index;
+              tabIndex = index;
             });
           },
         ),
@@ -101,4 +71,35 @@ class _AppPageState extends State<AppPage> with SingleTickerProviderStateMixin {
     super.dispose();
   }
 
+  Image getTabImage(path) {
+    return new Image.asset(path, width: 30.0, height: 30.0,fit: BoxFit.fill,);
+  }
+
+  Image getTabIcon(int curIndex) {
+    if (curIndex == tabIndex) {
+      return tabImages[curIndex][1];
+    }
+    return tabImages[curIndex][0];
+  }
+
+  void initData(){
+    tabImages = [
+      [
+        getTabImage('assets/images/app-home-icon.png'),
+        getTabImage('assets/images/app-home-icon.png'),
+      ],
+      [
+        getTabImage('assets/images/app-home-icon.png'),
+        getTabImage('assets/images/app-home-icon.png'),
+      ],
+      [
+        getTabImage('assets/images/app-home-icon.png'),
+        getTabImage('assets/images/app-home-icon.png'),
+      ],
+      [
+        getTabImage('assets/images/app-home-icon.png'),
+        getTabImage('assets/images/app-home-icon.png'),
+      ],
+    ];
+  }
 }
