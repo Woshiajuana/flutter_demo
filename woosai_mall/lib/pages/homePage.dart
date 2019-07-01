8175eed4c565ada2d94fbd3e9f426873536787e9
Woshiajuana <zhigang.chen@owulia.com>
2019-07-01 14:31:42 +0800

    sub code

--- woosai_mall/lib/pages/homePage.dart
+++ woosai_mall/lib/pages/homePage.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
 
@@ -12,10 +13,67 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Container(
-      child: new Center(
-        child: new Text('首页'),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('首页'),
+      ),
+      body: new Container(
+        child: new _CarouselView(),
       ),
     );
+  }
+}
+
+class _CarouselView extends StatefulWidget {
+  @override
+  _CarouselViewState createState() => _CarouselViewState();
+}
+
+class _CarouselViewState extends State<_CarouselView> {
+
+  // 声明一个list 存放image Widget
+  List<Widget> imageList = List();
+
+  @override
+  void initState() {
+    imageList
+      ..add(Image.network(
+        'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2726034926,4129010873&fm=26&gp=0.jpg',
+        fit: BoxFit.fill,
+      ))
+      ..add(Image.network(
+        'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3485348007,2192172119&fm=26&gp=0.jpg',
+        fit: BoxFit.fill,
+      ))
+      ..add(Image.network(
+        'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2594792439,969125047&fm=26&gp=0.jpg',
+        fit: BoxFit.fill,
+      ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      width: MediaQuery.of(context).size.width,
+      height: 200.0,
+      child: Swiper(
+        itemBuilder: _carouselBuilder,
+        itemCount: 3,
+        pagination: new SwiperPagination(
+          builder: DotSwiperPaginationBuilder(
+            color: Colors.black54,
+            activeColor: Colors.white,
+          ),
+        ),
+        controller: new SwiperController(),
+        scrollDirection: Axis.horizontal,
+        autoplay: true,
+        onTap: (index) => print('点击了第$index个'),
+      ),
+    );
+  }
+
+  Widget _carouselBuilder(BuildContext context, int index) {
+    return (imageList[index]);
   }
 }
