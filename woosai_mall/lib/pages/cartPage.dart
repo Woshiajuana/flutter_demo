dc4f8f118f5f5d29c5166327fce98241bb899d89
Woshiajuana <zhigang.chen@owulia.com>
2019-07-02 20:37:25 +0800

    sub flutter

--- woosai_mall/lib/pages/cartPage.dart
+++ woosai_mall/lib/pages/cartPage.dart
@@ -9,8 +9,19 @@ class CartPage extends StatelessWidget {
       appBar: new AppBar(
         title: new Text('购物车'),
       ),
-      body: new Container(
-        child: new Text('这是购物车页！'),
+      body: new GridView(
+          gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, //横轴三个子widget
+            childAspectRatio: 1.0 //宽高比为1时，子widget
+          ),
+          children:<Widget>[
+            Icon(Icons.ac_unit),
+            Icon(Icons.airport_shuttle),
+            Icon(Icons.all_inclusive),
+            Icon(Icons.beach_access),
+            Icon(Icons.cake),
+            Icon(Icons.free_breakfast)
+          ]
       ),
     );
   }
