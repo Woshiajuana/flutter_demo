bdcf4fc4a92ed955d532a7ba867d271a4ccd6917
Woshiajuana <zhigang.chen@owulia.com>
2019-07-02 11:28:24 +0800

    sub code and headView

---
+++ woosai_mall/lib/common/style/WowStyle.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+class WOWIcons {
+
+  static const TAO_BAO_FAMILY = 'taobaoIconFont';
+
+  static const IconData search = const IconData(0xe7da, fontFamily: TAO_BAO_FAMILY);
+
+}
--- woosai_mall/lib/pages/home/components/headView.dart
+++ woosai_mall/lib/pages/home/components/headView.dart
@@ -1,22 +1,50 @@
-
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:woosai_mall/common/style/WowStyle.dart';
 
-class HeadView extends StatelessWidget {
+
+class HeadView extends StatefulWidget {
+
+  @override
+  _HeadViewState createState() => new _HeadViewState();
+}
+
+class _HeadViewState extends State<HeadView> {
 
   @override
   Widget build(BuildContext context) {
     return Container(
+//      height: 50.0,
+//      color: Colors.red,
       color: Theme.of(context).primaryColor,
       padding: EdgeInsets.only(
         top: MediaQuery.of(context).padding.top,
-        left: 0,
-        right: 0,
-        bottom: 0,
+        left: 20.0,
+        right: 20.0,
+//        bottom: 20.0,
       ),
-      child: Row(
-        children: <Widget>[
-          new Text('哈哈')
-        ],
+      child: Card(
+        elevation: 10.0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20.0))),
+        child: Padding(
+          padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 5.0, top: 0, left: 5.0),
+                child: Icon(WOWIcons.search, color: Colors.grey, size: 20.0,),
+              ),
+              Expanded(
+                child: Text(
+                  '亲~点击窝可以更快找到心仪商品哦~~~',
+                  style: TextStyle(fontSize: 12.0, color: Colors.black26),
+                ),
+              ),
+            ],
+          ),
+        )
       ),
     );
   }
