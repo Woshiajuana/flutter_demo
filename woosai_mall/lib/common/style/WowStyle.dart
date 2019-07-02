7eb8e02cbb3ddfa2ea224f27ec5dccbd6e306149
Woshiajuana <zhigang.chen@owulia.com>
2019-07-02 13:58:14 +0800

    sub code

--- woosai_mall/lib/common/style/WowStyle.dart
+++ woosai_mall/lib/common/style/WowStyle.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 
 class WOWIcons {
 
-  static const TAO_BAO_FAMILY = 'taobaoIconFont';
+  static const String TAO_BAO_FAMILY = 'taobaoIconFont';
+
+  static const String logo100 = 'assets/images/app-logo-icon-100.png';
 
   static const IconData search = const IconData(0xe7da, fontFamily: TAO_BAO_FAMILY);
 
--- woosai_mall/lib/pages/home/components/headView.dart
+++ woosai_mall/lib/pages/home/components/headView.dart
@@ -13,39 +13,47 @@ class _HeadViewState extends State<HeadView> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-//      height: 50.0,
-//      color: Colors.red,
+    return new Container(
       color: Theme.of(context).primaryColor,
-      padding: EdgeInsets.only(
-        top: MediaQuery.of(context).padding.top,
-        left: 20.0,
-        right: 20.0,
-//        bottom: 20.0,
-      ),
-      child: Card(
-        elevation: 10.0,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20.0))),
-        child: Padding(
-          padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(right: 5.0, top: 0, left: 5.0),
-                child: Icon(WOWIcons.search, color: Colors.grey, size: 20.0,),
-              ),
-              Expanded(
-                child: Text(
-                  '亲~点击窝可以更快找到心仪商品哦~~~',
-                  style: TextStyle(fontSize: 12.0, color: Colors.black26),
+      padding: new EdgeInsets.only(left: 5.0, right: 5.0),
+      child: new Row(
+        children: <Widget>[
+          new Image.asset(
+            WOWIcons.logo100,
+            width: 36.0,
+            height: 36.0,
+            fit: BoxFit.fill,
+            color: Colors.white,
+          ),
+          new Expanded(
+            flex: 1,
+            child: new Card(
+              elevation: 0,
+              shape: new RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              child: new Padding(
+                padding: new EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new Padding(
+                      padding: new EdgeInsets.only(right: 5.0, top: 0, left: 5.0),
+                      child: new Icon(WOWIcons.search, color: Colors.grey, size: 20.0,),
+                    ),
+                    new Expanded(
+                      child: new Text(
+                        '亲~点击窝可以更快找到心仪商品哦~~~',
+                        style: new TextStyle(fontSize: 12.0, color: Colors.black26),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        )
+            ),
+          )
+        ],
       ),
     );
   }
 }
+
