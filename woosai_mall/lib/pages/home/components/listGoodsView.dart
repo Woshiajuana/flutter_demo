86c4e454c72b0d26a9f02237e64906d2d26b774e
Woshiajuana <zhigang.chen@owulia.com>
2019-07-05 19:59:08 +0800

    sub code

--- woosai_mall/lib/pages/home/components/listGoodsView.dart
+++ woosai_mall/lib/pages/home/components/listGoodsView.dart
@@ -64,16 +64,6 @@ class _ListGoodsViewState extends State<ListGoodsView> {
       ),
     );
   }
-  static SlideTransition createTransition(
-      Animation<double> animation, Widget child) {
-    return new SlideTransition(
-      position: new Tween<Offset>(
-        begin: const Offset(1.0, 0.0),
-        end: const Offset(0.0, 0.0),
-      ).animate(animation),
-      child: child,
-    );
-  }
 
   List<String> getDataList() {
     List<String> list = [];
