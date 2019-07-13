b73ccdd14c3ff27ee8fbb1251ed8ccf62bbdb725
Woshiajuana <zhigang.chen@owulia.com>
2019-07-13 16:22:18 +0800

    sub flutter

--- woosai_mall/lib/pages/minePage.dart
+++ woosai_mall/lib/pages/minePage.dart
@@ -1,7 +1,13 @@
 
 import 'package:flutter/material.dart';
 
-class MinePage extends StatelessWidget {
+class MinePage extends StatefulWidget {
+
+  @override
+  _MinePageState createState() => new _MinePageState();
+}
+
+class _MinePageState extends State<MinePage> {
 
   @override
   Widget build(BuildContext context) {
