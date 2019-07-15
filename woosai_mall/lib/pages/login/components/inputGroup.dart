3d4f041aebebdd881f10ef66dd216253112daa29
Woshiajuana <zhigang.chen@owulia.com>
2019-07-15 16:48:22 +0800

    sub inputgroup

--- woosai_mall/lib/pages/login/components/inputGroup.dart
+++ woosai_mall/lib/pages/login/components/inputGroup.dart
@@ -16,20 +16,21 @@ class _InputGroupState extends State<InputGroup> {
       padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 32.0),
       child: new Column(
         children: <Widget>[
-          _inputItem(),
-          _inputItem(),
+          _inputItem('账号', false),
+          _inputItem('密码', true),
           _switchItem(),
         ],
       ),
     );
   }
 
-  Widget _inputItem () {
+  Widget _inputItem (String labelText, bool obscureText) {
     return new Container(
       margin: const EdgeInsets.only(top: 16.0),
       child: new TextField(
+        obscureText: obscureText,
         decoration: new InputDecoration(
-          labelText: '账号',
+          labelText: labelText,
         ),
       ),
     );
