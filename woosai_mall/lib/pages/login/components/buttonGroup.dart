58a5e59fa758e7909c7b3696d8eb8208619ad1b2
Woshiajuana <zhigang.chen@owulia.com>
2019-07-15 19:44:14 +0800

    sub code new container

--- woosai_mall/lib/pages/login/components/buttonGroup.dart
+++ woosai_mall/lib/pages/login/components/buttonGroup.dart
@@ -12,24 +12,28 @@ class _ButtonGroupState extends State<ButtonGroup> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Container(
-      height: 45.0,
-      margin: const EdgeInsets.only(left: 60.0, right: 60.0, top: 60.0),
-      decoration: new BoxDecoration(
-        color: Colors.blue,
-        borderRadius: new BorderRadius.circular(30.0),
-      ),
-      child: new Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          new Text(
-            '登录',
-            style: new TextStyle(
-              color: Colors.white,
-              fontSize: 16.0,
-            ),
+    return new Material(
+      child: new InkWell(
+        child: new Container(
+          height: 45.0,
+          margin: const EdgeInsets.only(left: 60.0, right: 60.0, top: 60.0),
+          decoration: new BoxDecoration(
+            color: Colors.blue,
+            borderRadius: new BorderRadius.circular(30.0),
           ),
-        ],
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(
+                '登录',
+                style: new TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
