732cbb2d2e3f7348fd7c9432f4b1bd8c208087d6
Woshiajuana <zhigang.chen@owulia.com>
2019-07-16 16:17:22 +0800

    sub storeutil

--- woosai_mall/lib/common/utils/storeUtil.dart
+++ woosai_mall/lib/common/utils/storeUtil.dart
@@ -1,6 +1,32 @@
 
+import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class StoreUtil {
-  
+  // 保存
+  static save(String key, dynamic value) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    if (value is Map) {
+      value = json.encode(value);
+    }
+    sharedPreferences.setString(key, value);
+  }
+
+  // 取出
+  static get(String key) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    var value = sharedPreferences.get(key);
+    try {
+      value = json.decode(value);
+    } catch (e) {
+      print(e);
+    }
+    return value;
+  }
+
+  // 删除
+  static remove(key) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.remove(key);
+  }
 }
