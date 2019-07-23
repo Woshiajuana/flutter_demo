2993cefcae92ddb8eeff4c461e9f12cca47fd387
Woshiajuana <zhigang.chen@owulia.com>
2019-07-23 20:07:58 +0800

    sub storeutil

--- woosai_mall/lib/utils/store.util.dart
+++ woosai_mall/lib/utils/store.util.dart
@@ -1,13 +1,53 @@
 
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
 class Store {
 
-  static final Config _config = Config._internal();
+  static final Store _store = Store._internal();
 
-  factory Config () {
-    return _config;
+  factory Store () {
+    return _store;
   }
 
-  Config._internal();
+  Store._internal();
 
+  static SharedPreferences _sharedPreferences;
+
+  static Future _init() async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  Future get (String key) async {
+    if (_sharedPreferences == null) {
+      await _init();
+    }
+    var value = _sharedPreferences.get(key);
+    try {
+      value = json.decode(value);
+    } catch (err) {
+
+    }
+    return value;
+  }
+
+  // 保存
+  Future set (String key, dynamic value) async {
+    if (_sharedPreferences == null) {
+      await _init();
+    }
+    if (value is Map) {
+      value = json.encode(value);
+    }
+    await  _sharedPreferences.setString(key, value);
+  }
+
+  // 删除
+  Future remove (String key) async {
+    if (_sharedPreferences == null) {
+      await _init();
+    }
+    await _sharedPreferences.remove(key);
+  }
 
 }
