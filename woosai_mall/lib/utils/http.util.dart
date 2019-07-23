bc2059942893d8ce946aa538977df495e4a07b95
Woshiajuana <zhigang.chen@owulia.com>
2019-07-23 20:16:23 +0800

    sub code and http.util

--- woosai_mall/lib/utils/http.util.dart
+++ woosai_mall/lib/utils/http.util.dart
@@ -1,5 +1,26 @@
 
+import 'dart:io';
+import 'package:dio/dio.dart';
 
 class Http {
+
+  static final Http _http = Http._internal();
+
+  factory Http () {
+    return _http;
+  }
+
+  Http._internal();
+
+  static Dio _dio;
+
+
+
+  static BaseOptions _options = new BaseOptions(
+    method: 'POST',
+    connectTimeout: 1000 * 10,
+    receiveTimeout: 1000 * 20,
+  );
+
 
 }
