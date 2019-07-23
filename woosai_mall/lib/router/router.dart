34c34ad70e1e1846e7ee98d0ed46790e2613e124
Woshiajuana <zhigang.chen@owulia.com>
2019-07-23 16:58:50 +0800

    sub code and router

--- woosai_mall/lib/router/router.dart
+++ woosai_mall/lib/router/router.dart
@@ -1,13 +1,25 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-
 import 'package:woosai_mall/pages/appPage.dart';
 import 'package:woosai_mall/pages/welcome/welcomePage.dart';
 import 'package:woosai_mall/pages/login/loginPage.dart';
+import 'package:woosai_mall/pages/list/listPage.dart';
+import 'package:woosai_mall/pages/details/detailsPage.dart';
+import 'package:woosai_mall/pages/confirm/confirmPage.dart';
 import 'package:woosai_mall/pages/address/addressPage.dart';
+import 'package:woosai_mall/pages/address/addressInfoPage.dart';
+import 'package:woosai_mall/pages/login/loginPage.dart';
 
 class Router {
+
+  static final Router _router = Router._internal();
+
+  factory Router () {
+    return _router;
+  }
+
+  Router._internal();
 
   static Map<String, Map<String, dynamic>> config = {
     '/': {
@@ -38,8 +50,8 @@ class Router {
     return routes;
   }
 
-  push (BuildContext context) {
-    return navigatorRouter(context, new AddressPage());
+  push (BuildContext context, path, { params }) {
+    return navigatorRouter(context, config[path]['handle']());
   }
 
   // 修改路由动画
