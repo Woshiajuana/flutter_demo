06e4a8f5d85738c6ceee290508548ad5fe1ade5b
Woshiajuana <zhigang.chen@owulia.com>
2019-07-24 09:43:43 +0800

    sub http.util and dart

--- woosai_mall/lib/utils/http.util.dart
+++ woosai_mall/lib/utils/http.util.dart
@@ -13,10 +13,36 @@ class Http {
   Http._internal();
 
   static Dio _dio;
+  static BaseOptions _options = new BaseOptions(
+    method: 'POST',
+    connectTimeout: 1000 * 10,
+    receiveTimeout: 1000 * 20,
+  );
 
   static Future _init() async {
-    _dio = await SharedPreferences.getInstance();
+    _dio = new Dio(_options);
+    _dio.interceptors
+    .add(InterceptorsWrapper(onRequest: (RequestOptions options) {
+
+    }, onResponse: (Response response){
+
+    }, onError: (DioError dioErr) {
+
+    }));
   }
 
+  Future get (String url, {Map<String, dynamic> params}) async {
+    if (_dio == null) {
+      await _init();
+    }
+    return await _dio.get(url, queryParameters: params);
+  }
+
+  Future post (String url, {Map<String, dynamic> params}) async {
+    if (_dio == null) {
+      await _init();
+    }
+    return await _dio.post(url, data: params);
+  }
 
 }
