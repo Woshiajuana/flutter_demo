7818e548d875627c8e9ebef2acd03057bbc55d97
Woshiajuana <zhigang.chen@owulia.com>
2019-07-25 14:36:49 +0800

    sub code

--- woosai_mall/lib/utils/loading.util.dart
+++ woosai_mall/lib/utils/loading.util.dart
@@ -1,6 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading {
 
@@ -14,22 +13,34 @@ class Loading {
 
   static BuildContext _context;
 
-  void show (BuildContext context, { Function callback }) {
+  void show (BuildContext context, { Function callback, bool barrierDismissible = true }) {
     if (_context != null) return;
     _context = context;
     showDialog(
+      barrierDismissible: false,
       context: _context,
       builder: (context) {
         return _widgetLoading();
       },
     ).then((value) {
-      if (callback != null) {
-        callback(value);
-      }
+      print('xsaxasxasxaxaxaxaxaxaxasxx');
+//      _hide();
+//      if (callback != null) {
+//        callback(value);
+//      }
     });
   }
 
   void hide () {
+    print('执行hide=hidehidehidehidehidehidehidehidehidehide');
+    if (_context != null) {
+      Navigator.pop(_context);
+      _context = null;
+    }
+  }
+
+  static _hide () {
+    print('执行_hide=====================');
     if (_context != null) {
       Navigator.pop(_context);
       _context = null;
@@ -37,16 +48,19 @@ class Loading {
   }
 
   static Widget _widgetLoading () {
-    return new Center(
-      child: new Container(
-        width: 80.0,
-        height: 80.0,
-        decoration: new BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        child: new Center(
-          child: new CircularProgressIndicator(),
+    return new WillPopScope(
+      onWillPop: () => _hide(),
+      child: new Center(
+        child: new Container(
+          width: 80.0,
+          height: 80.0,
+          decoration: new BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          child: new Center(
+            child: new CircularProgressIndicator(),
+          ),
         ),
       ),
     );
