a10f73721ad637224a6130ff52ea021956faaac1
Woshiajuana <zhigang.chen@owulia.com>
2019-07-25 10:28:00 +0800

    sub code and loginpage and flluter

--- woosai_mall/lib/pages/login/loginPage.dart
+++ woosai_mall/lib/pages/login/loginPage.dart
@@ -5,10 +5,9 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:woosai_mall/pages/login/components/headerGroup.dart';
 import 'package:woosai_mall/pages/login/components/inputGroup.dart';
 import 'package:woosai_mall/pages/login/components/buttonGroup.dart';
-import 'package:woosai_mall/common/utils/apiUtil.dart';
-import 'package:woosai_mall/common/utils/routerUtil.dart';
-import 'package:woosai_mall/common/redux/appState.dart';
-import 'package:woosai_mall/common/mixins/userMixin.dart';
+import 'package:woosai_mall/models/userInfo.model.dart';
+import 'package:woosai_mall/redux/app.redux.dart';
+import 'package:woosai_mall/redux/userInfo.redux.dart';
 import 'package:woosai_mall/application.dart';
 
 class LoginPage extends StatefulWidget {
@@ -19,7 +18,7 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => new _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> with UserMixin {
+class _LoginPageState extends State<LoginPage> {
 
   String _username = '13127590698';
   String _password = '111111';
@@ -50,16 +49,14 @@ class _LoginPageState extends State<LoginPage> with UserMixin {
   // 提交
   void _handleSubmit(Store store) async {
     try {
-      var respBody = await Application.service.user.doUserLogin(phone: _username, password: _password);
-
-
+      var userInfoJson = await Application.service.user.doUserLogin(phone: _username, password: _password);
+      String userInfoJsonKey = Application.config.store.userInfoJson;
+      await Application.util.store.set(userInfoJsonKey, userInfoJson);
+      UserInfo userInfo = UserInfo.fromJson(userInfoJson);
+      store.dispatch(new UpdateUserInfoAction(userInfo));
+      Application.router.push(context, 'app');
     } catch (err) {
       Application.util.modal.toast(err);
     }
-
-//    var respBody = await ApiUtil.userLogin(_username, _password);
-//    if (respBody == null) return;
-//    await userUpdateInfo(store, respBody);
-//    RouterUtil.pushApp(context);
   }
 }
