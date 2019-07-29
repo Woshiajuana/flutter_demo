915f66653d6bea9235ec04ee865813f62ccf14b0
Woshiajuana <zhigang.chen@owulia.com>
2019-07-29 16:35:37 +0800

    sub orderpage

--- woosai_mall/lib/pages/order/orderPage.dart
+++ woosai_mall/lib/pages/order/orderPage.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:woosai_mall/common/application.dart';
 
 class OrderPage extends StatefulWidget {
   @override
@@ -17,7 +18,7 @@ class OrderPageState extends State<OrderPage> {
       ),
       body: new ListView(
         children: <Widget>[
-
+          _widgetOrderCellItem(),
         ],
       ),
     );
@@ -31,7 +32,97 @@ class OrderPageState extends State<OrderPage> {
 
   Widget _widgetOrderCellItem () {
     return new Container(
-      
+      child: new Column(
+        children: <Widget>[
+          _widgetOrderGoods(),
+          new Container(
+            child: new Row(
+              children: <Widget>[
+                new Text(
+                  '待支付',
+                  style: new TextStyle(
+                    fontSize: 16.0,
+                    color: Color(0xff333333),
+                  ),
+                ),
+                new Container(
+
+                  child: new FlatButton(
+                    onPressed: () => {},
+                    child: new Text(
+                      '查看详情',
+                      style: new TextStyle(
+                        fontSize: 16.0,
+                        color: Color(0xff333333),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _widgetOrderGoods () {
+    return new Container(
+      child: new Row(
+        children: <Widget>[
+          new Container(
+            child: new FadeInImage.assetNetwork(
+              placeholder: Application.config.style.srcGoodsNull,
+              image: '',
+            ),
+          ),
+          new Expanded(
+            child: new Container(
+              child: new Column(
+                children: <Widget>[
+                  new Container(
+                    child: new Text(
+                      '测试商品',
+                      style: new TextStyle(
+                        fontSize: 16.0,
+                        color: Color(0xff333333),
+                      ),
+                    ),
+                  ),
+                  new Expanded(flex: 1, child: new Container()),
+                  new Row(
+                    children: <Widget>[
+                      new Text(
+                        '￥1111',
+                        style: new TextStyle(
+                          fontSize: 13.0,
+                          color: Color(0xff999999),
+                        ),
+                      ),
+                      new Expanded(flex: 1, child: new Container()),
+                      new Text(
+                        '￥1111',
+                        style: new TextStyle(
+                          fontSize: 13.0,
+                          color: Color(0xff999999),
+                        ),
+                      ),
+                      new Text(
+                        '￥1111',
+                        style: new TextStyle(
+                          fontSize: 13.0,
+                          color: Color(0xff333333),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+
+        ],
+      ),
     );
   }
 
